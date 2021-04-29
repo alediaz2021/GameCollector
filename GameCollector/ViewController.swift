a4109b17399febc917f45296ff381efc04bae559
Alexx <>
2021-04-28 23:55:51 -0400

    Initial commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Alejandro Diaz Sotolongo on 4/28/21.
+//  Copyright © 2021 aaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
